ee29c58497bb25302c521ad4eaa7c19559d04685
Mthohdisane <mthobisimshika55@gmail.com>
2021-12-22 23:27:36 -0800

    Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+
+Attribute lookup
+
+"""
+
+
+
+
+
+def lookup(obj):
+
+    """Lookup all attributes in Class
+
+
+
+    Args:
+
+        obj: object class
+
+    Returns:
+
+        Na
+
+    """
+
+    return [i for i in dir(obj)]
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+
+empty class module
+
+"""
+
+
+
+
+
+class BaseGeometry:
+
+    """empty class"""
+
+
+
+    pass
